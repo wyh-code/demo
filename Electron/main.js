1f5b254ff6952484ff98845cb8285007e8a22475
吴永辉 <wb-wyh386287@alibaba-inc.com>
2024-09-19 14:55:14 +0800

    electron-2-生命周期

--- Electron/main.js
+++ Electron/main.js
@@ -15,9 +15,27 @@ const createWindow = () => {
 
   // 打开控制台
   win.webContents.openDevTools();
+
+  // 生命周期
+  win.webContents.on('dom-ready', () => {
+    console.log('2-dom-ready')
+  })
+
+  win.webContents.on('did-finish-load', () => {
+    console.log('3-did-finish-load: 窗口页面dom加载完毕')
+  })
+
+  win.on('close', () => {
+    console.log('4-close: 窗口关闭')
+  })
+
+  win.on('closed', () => {
+    console.log('closed: 窗口关闭')
+  })
 }
 
 app.whenReady().then(() => {
+  console.log('1-ready')
   // 创建窗口
   createWindow();
 
@@ -25,10 +43,24 @@ app.whenReady().then(() => {
     console.log('close')
   });
 
+  // 当监听 window-all-closed 生命周期后，需要手动执行 app.quit() 才会触发quit相关生命周期
   app.on('window-all-closed', () => {
+    console.log('5-window-all-closed: 所有窗口都关闭');
     // 不是macOS系统，当所有窗口被关闭，则退出应用
     if(process.platform !== 'darwin') app.quit();
   });
+
+  app.on('before-quit', () => {
+    console.log('6-before-quit: 应用退出前');
+  })
+
+  app.on('will-quit', () => {
+    console.log('7-will-quit: 应用退出');
+  })
+
+  app.on('quit', () => {
+    console.log('8-will-quit: 应用已经退出');
+  })
 
   app.on('activate', () => {
     /**
